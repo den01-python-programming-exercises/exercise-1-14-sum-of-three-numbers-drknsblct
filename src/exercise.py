a63b1c9f8f6f93f5e95059950d009f09039cb654
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-17 01:53:17 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    first = int(input('Give the first number:'))
+    second = int(input('Give the second number:'))
+    third = int(input('Give the third number:'))
+
+#     print(f'The sum of the numbers is {first + second}')
+#     flake8 doesn't understand f-strings for some reason...
+
+    print('The sum of the numbers is ' + str(first + second + third))
 
 if __name__ == '__main__':
     main()
